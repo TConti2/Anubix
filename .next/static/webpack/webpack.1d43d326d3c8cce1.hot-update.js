b7401e26bc4a849bc5cd0260d5da60c6f25dcd94
Taylor Conti <conti_taylor@icloud.com>
2026-05-23 20:02:51 -0500

    Add protected payments dashboard with balance editing

--- .next/static/webpack/webpack.1d43d326d3c8cce1.hot-update.js
+++ .next/static/webpack/webpack.1d43d326d3c8cce1.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("8d1b538d75111082")
+/******/ 	__webpack_require__.h = () => ("c949a3f92bf2efaa")
 /******/ })();
 /******/ 
 /******/ }
