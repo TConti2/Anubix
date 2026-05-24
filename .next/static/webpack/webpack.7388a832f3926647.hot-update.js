6dad3b1ed0abf218425dbb9e68db31dadf895355
Taylor Conti <conti_taylor@icloud.com>
2026-05-23 22:27:13 -0500

    Add parent portal progress and finance visibility

--- .next/static/webpack/webpack.7388a832f3926647.hot-update.js
+++ .next/static/webpack/webpack.7388a832f3926647.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("8542d6cf515f0e1d")
+/******/ 	__webpack_require__.h = () => ("d6a0ebd86d70b6fd")
 /******/ })();
 /******/ 
 /******/ }
